58313eca87e0cd4311d8b407c6b90323fdc7599c
Gabriel Barta <gbarta@gabrielbarta.com>
2015-02-27 14:03:08 +1100

    Starting spacemacs

--- gbarta/extensions.el
+++ gbarta/extensions.el
@@ -1,32 +1,23 @@
 ;;; extensions.el --- gbarta Layer extensions File for Spacemacs
 ;;
-;; Copyright (c) 2012-2014 Sylvain Benner
-;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
-;;
-;; Author: Sylvain Benner <sylvain.benner@gmail.com>
-;; URL: https://github.com/syl20bnr/spacemacs
-;;
 ;; This file is not part of GNU Emacs.
 ;;
-;;; License: GPLv3
 
 (defvar gbarta-pre-extensions
   '(
-    ;; pre extension gbartas go here
     )
   "List of all extensions to load before the packages.")
 
 (defvar gbarta-post-extensions
   '(
-    ;; post extension gbartas go here
+    evil-noautochdir
     )
   "List of all extensions to load after the packages.")
 
-;; For each extension, define a function gbarta/init-<extension-gbarta>
-;;
-;; (defun gbarta/init-my-extension ()
-;;   "Initialize my extension"
-;;   )
+(defun gbarta/init-evil-noautochdir ()
+  "Initialize my extension"
+  (require 'evil-noautochdir)
+  )
 ;;
 ;; Often the body of an initialize function uses `use-package'
 ;; For more info on `use-package', see readme:
